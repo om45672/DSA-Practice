51d5ca71edbd691828ed8ed160fd3f62097612cd
om45672 <patelom927@gmail.com>
2025-03-16 19:09:49 +0530

    Create day14-Problem-3.cpp

---
+++ DSA_Practice/day14-Problem-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low = 1;
+        int high = *max_element(nums.begin(),nums.end());
+        int ans = -1;
+        while(low<=high) {
+            int mid = low + (high-low)/2;
+            int sum = 0;
+            for(auto it:nums) sum+=ceil((double)(it)/(double)(mid));
+            if(sum<=threshold){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+   
+    return ans;
+    }
+};
