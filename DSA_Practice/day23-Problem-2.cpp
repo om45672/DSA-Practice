44470934f59f18a4820a8e1bafc19eddf620cc16
om45672 <patelom927@gmail.com>
2025-03-25 18:47:33 +0530

    Update day23-Problem-2.cpp

--- DSA_Practice/day23-Problem-2.cpp
+++ DSA_Practice/day23-Problem-2.cpp
@@ -2,24 +2,22 @@ Time Limit Exceeded 125 / 126 testcases passed
 
 class Solution {
 public:
-    bool is_palindrome(const string& s, int end) {
-        int left = 0, right = end;
-        while (left < right) {
-            if (s[left++] != s[right--]) {
-                return false;
-            }
+    bool is_palindrome(const string& temp){
+        int n = temp.size();
+        for(int i=0;i<n/2;i++){
+            if(temp[i]!=temp[n-i-1]) return false;
         }
         return true;
     }
-
     string shortestPalindrome(string s) {
-        int n = s.size();
-        int i = n - 1;
-        while (i >= 0 && !is_palindrome(s, i)) {
-            i--;
+        ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+        string temp = s;
+        string add = "";
+        while(!is_palindrome(temp)){
+            add+=temp.back();
+            temp.pop_back();
         }
-        string add = s.substr(i + 1);
-        reverse(add.begin(), add.end());
-        return add + s;
+        return add+s;
     }
 };
