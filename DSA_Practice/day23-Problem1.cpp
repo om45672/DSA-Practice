98f20b17f8ae7eba85eb59f704c44c4e3588a990
om45672 <patelom927@gmail.com>
2025-03-25 18:43:13 +0530

    Update day23-Problem1.cpp

--- DSA_Practice/day23-Problem1.cpp
+++ DSA_Practice/day23-Problem1.cpp
@@ -3,17 +3,11 @@ public:
     int repeatedStringMatch(string a, string b) {
         int count = 1;
         string repeated = a;
-        
-        // Repeat "a" until its length is at least that of "b"
         while (repeated.size() < b.size()) {
             repeated += a;
             count++;
         }
-        
-        // Check if "b" is a substring of the current repeated string
         if (repeated.find(b) != string::npos) return count;
-
-        // Append one more "a" and check again
         repeated += a;
         count++;
         if (repeated.find(b) != string::npos) return count;
