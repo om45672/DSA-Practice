d02299e124e1cc10a4fd9a410dbfc0ef004f3f81
om45672 <patelom927@gmail.com>
2025-03-25 19:39:24 +0530

    Update day23-Problem-2.cpp

--- DSA_Practice/day23-Problem-2.cpp
+++ DSA_Practice/day23-Problem-2.cpp
@@ -1,4 +1,43 @@
-Time Limit Exceeded 125 / 126 testcases passed
+/* Using KMP algorithm */
+
+class Solution {
+public:
+    void lps(vector<int>&  lp, string a){
+        int i=0,j=1;
+        while(j<a.length()){
+            if(a[i]==a[j]){
+                i++;
+                lp[j]=i;
+                j++;
+            }
+            else{
+                if(i==0){
+                    j++;
+                }
+                else{
+                    i=lp[i-1];
+                }
+            }
+        }
+    }
+    string shortestPalindrome(string s) {
+        string copy = s;
+        reverse(copy.begin(),copy.end());
+        string a = s+'#'+copy;
+        vector<int> lp(a.length(),0);
+        lps(lp,a);
+        int n = lp[a.length()-1];
+        string temp = s.substr(n);
+        reverse(temp.begin(),temp.end());
+        return temp+s;
+    }
+};
+
+
+/* 
+    ALTERNATE APPROACH        
+Time Limit Exceeded 125 / 126 testcases passed 
+*/
 
 class Solution {
 public:
