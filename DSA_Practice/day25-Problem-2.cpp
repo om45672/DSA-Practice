3f843809b487fc858992c49651cf8eee5efe9336
om45672 <patelom927@gmail.com>
2025-03-28 10:50:29 +0530

    Create day25-Problem-2.cpp

---
+++ DSA_Practice/day25-Problem-2.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+  public:
+    Node *insertAtEnd(Node *head, int x) {
+        if(head == NULL) return new Node(x);
+       Node* temp = head;
+       while(temp->next){
+           temp = temp->next;
+       }
+       Node* newNode = new Node(x);
+       temp->next = newNode;
+       return head;
+    }
+};
